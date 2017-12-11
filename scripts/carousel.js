4d88fe1aeebc0d46ff8cf06746446e190fd90aad
Nicolas Weisslinger <weiss.weisslinger@gmail.com>
2017-12-11 11:27:53 +0100

    1st commit

---
+++ scripts/carousel.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        arrows: true,
+        dots: true,
+        slidesToShow: 1,
+        autoplay:true,
+        autoplaySpeed:3000,
+        pauseOnHover:false,
+        pauseOnFocus:false
+        
+    });
+});
